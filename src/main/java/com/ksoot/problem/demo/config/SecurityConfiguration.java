2c1a658deec53df004f6522387d102779750089a
officiallysingh <raj14.1984@gmail.com>
2023-12-22 11:41:08 +0530

    Updated Spring boot version to 3.2.0

--- src/main/java/com/ksoot/problem/demo/config/SecurityConfiguration.java
+++ src/main/java/com/ksoot/problem/demo/config/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.lang.Nullable;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,51 +23,59 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 @EnableMethodSecurity // Allow method annotations like @PreAuthorize, not required as of now
 class SecurityConfiguration {
 
-  private final String[] SWAGGER_URLS = new String[]{"/swagger-resources/**", "/swagger-ui/**", "/swagger-ui.*", "/v3/api-docs", "/v3/api-docs/**", "/webjars/**"};
+    private final String[] SWAGGER_URLS = new String[]{"/swagger-resources/**", "/swagger-ui/**", "/swagger-ui.*", "/v3/api-docs", "/v3/api-docs/**", "/webjars/**"};
 
-  private boolean actuatorBypassSecurity = true;
+    private boolean actuatorBypassSecurity = true;
 
-  private final AuthenticationEntryPoint authenticationEntryPoint;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
 
-  private final AccessDeniedHandler accessDeniedHandler;
+    private final AccessDeniedHandler accessDeniedHandler;
 
-  private final ActuatorEndpointProperties actuatorEndpointProperties;
+    private final ActuatorEndpointProperties actuatorEndpointProperties;
 
-  SecurityConfiguration(@Nullable final AuthenticationEntryPoint authenticationEntryPoint,
-                        @Nullable final AccessDeniedHandler accessDeniedHandler,
-                        @Nullable final ActuatorEndpointProperties actuatorEndpointProperties) {
-    this.authenticationEntryPoint = authenticationEntryPoint;
-    this.accessDeniedHandler = accessDeniedHandler;
-    this.actuatorEndpointProperties = actuatorEndpointProperties;
-  }
-
-  @Bean
-  SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
-    // @formatter:off
-    http.csrf().disable()
-        .authorizeHttpRequests((requests) -> requests
-            .requestMatchers(this.actuatorBypassSecurity && this.actuatorEndpointProperties != null
-                ? ArrayUtils.addAll(SWAGGER_URLS, this.actuatorEndpointProperties.getPaths())
-                : SWAGGER_URLS).permitAll()
-            .requestMatchers("/problems/**", "/**/pets/**").permitAll()
-            .anyRequest().authenticated()
-        ).oauth2ResourceServer()
-        .jwt().jwtAuthenticationConverter(this.jwtAuthenticationConverter()).decoder(new JwtStringDecoder());
-
-    if (this.authenticationEntryPoint != null) {
-      http.exceptionHandling().authenticationEntryPoint(this.authenticationEntryPoint);
+    SecurityConfiguration(@Nullable final AuthenticationEntryPoint authenticationEntryPoint,
+                          @Nullable final AccessDeniedHandler accessDeniedHandler,
+                          @Nullable final ActuatorEndpointProperties actuatorEndpointProperties) {
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.accessDeniedHandler = accessDeniedHandler;
+        this.actuatorEndpointProperties = actuatorEndpointProperties;
     }
-    if (this.accessDeniedHandler != null) {
-      http.exceptionHandling().accessDeniedHandler(this.accessDeniedHandler);
-    }
-    // @formatter:on
-    return http.build();
-  }
 
-  private JwtAuthenticationConverter jwtAuthenticationConverter() {
-    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(JwtUtils.jwtGrantedAuthoritiesConverter());
-    jwtAuthenticationConverter.setPrincipalClaimName(JwtUtils.PRINCIPLE_NAME_CLAIM_ID);
-    return jwtAuthenticationConverter;
-  }
+    @Bean
+    SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        // @formatter:off
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests((requests) -> requests
+                .requestMatchers(this.actuatorBypassSecurity && this.actuatorEndpointProperties != null
+                    ? ArrayUtils.addAll(SWAGGER_URLS, this.actuatorEndpointProperties.getPaths())
+                    : SWAGGER_URLS).permitAll()
+                .requestMatchers("/problems/**", "/**/pets/**").permitAll()
+                .anyRequest().authenticated()
+            ).oauth2ResourceServer(
+                resourceServerCustomizer ->
+                        resourceServerCustomizer.jwt(
+                                jwtCustomizer ->
+                                        jwtCustomizer
+                                                .jwtAuthenticationConverter(this.jwtAuthenticationConverter())
+                                                .decoder(new JwtStringDecoder())));
+
+        if (this.authenticationEntryPoint != null) {
+          http.exceptionHandling(
+                  exceptionHandling ->
+                          exceptionHandling.authenticationEntryPoint(this.authenticationEntryPoint));
+        }
+        if (this.accessDeniedHandler != null) {
+          http.exceptionHandling(
+                  exceptionHandling -> exceptionHandling.accessDeniedHandler(this.accessDeniedHandler));
+        }
+        // @formatter:on
+        return http.build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(JwtUtils.jwtGrantedAuthoritiesConverter());
+        jwtAuthenticationConverter.setPrincipalClaimName(JwtUtils.PRINCIPLE_NAME_CLAIM_ID);
+        return jwtAuthenticationConverter;
+    }
 }
